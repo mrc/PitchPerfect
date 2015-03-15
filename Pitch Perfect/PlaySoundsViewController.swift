cd1a690f93df35dc2cf4fe41656da6a138750564
Matt Curtis <matt.r.curtis@gmail.com>
2015-03-15 23:52:19 +1100

    Play the movie quote (slow and fast).

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Matt Curtis on 15/03/2015.
+//  Copyright (c) 2015 Matt Curtis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let path = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            let url = NSURL(fileURLWithPath: path)
+            
+            AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+            AVAudioSession.sharedInstance().setActive(true, error: nil)
+            
+            var error: NSError?
+            audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
+            audioPlayer.enableRate = true
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSoundSlowly(sender: UIButton) {
+        audioPlayer.rate = 0.5
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+    
+    @IBAction func playSoundQuickly(sender: AnyObject) {
+        audioPlayer.rate = 2.0
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+}
