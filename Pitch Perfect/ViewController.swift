f7c9aea97a9200614a8687a0e158e80ee4b68189
Matt Curtis <matt.r.curtis@gmail.com>
2015-03-15 19:04:11 +1100

    Added basic functionality for initial view.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        recordingInProgress.hidden = true
+        stopButton.hidden = true
+        recordButton.enabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+        
+        //TODO: Record audio from the microphone
+        println("in recordAudio")
+    }
 }
 
